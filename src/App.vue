4bc840ae1887b29fdbace9ecee7f4eebe5b7f4c8
nseulgee <nseulgee@gmail.com>
2024-05-12 11:57:59 +0000

    기본 속성 및 이벤트 바인딩, 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,68 @@
 <template>
-    <h1>{{ message }}</h1>
+    <BasicBind />
+    
+    <EventBind />
 </template>
 
 <script>
+import BasicBind from '~/components/BasicBind';
+import EventBind from '~/components/EventBind';
+
 export default {
+    //현재 컴포넌트에서 사용할 컴포넌트 명시
+    components: {
+        //BasicBind: BasicBind // (O)
+        BasicBind,             // (O)
+        EventBind,
+    },
+    // 사용할 데이터 명시
     data() {
         return {
-            message: "Hello Vue!"
+        }
+    },
+}
+</script>
+
+<!-- NOTE: CSS 속성
+    lang: 전처리기 언어를 선언
+    scoped: 스타일을 선언된 컴포넌트에서만 사용
+-->
+<style lang="scss">
+section {
+    border-radius: 10px;
+    background-color: aliceblue;
+    margin: 10px 20px;
+    padding: 10px;
+}
+h1 {
+    margin: 0 20px;
+    font-size: 50px;
+}
+h2 {
+    margin: 15px 40px;
+    font-size: 40px;
+}
+ul{
+    margin: 15px 50px;
+    li{
+        list-style: inside;
+        font-size: 30px;
+        ul{
+            li{
+                color: orange;
+            }
         }
     }
 }
-</script>
+.btn{
+    display: inline-block;
+    padding: 3px 10px;
+    border: 1px solid salmon;
+    border-radius: 5px;
+    background-color: #fff;
+    color: salmon;
+    font-size: .8em;
+    margin-bottom: 0.1em;
+    cursor: pointer;
+}
+</style>
---
+++ src/components/EventBind.vue
@@ -0,0 +1,59 @@
+<template>
+    <!-- NOTE: 템플릿 문법
+        v-on  : method 에 선언된 함수를 이벤트와 바인딩 시켜준다.
+            -★@이벤트="함수명"     : v-on 생략 형태
+            -★@[변수명]="함수명"   : 이벤트 부분 또한 동적으로 데이터를 전달받을 시 사용
+    -->
+    <section>
+        <h1>이벤트 바인딩</h1>
+        <ul class="event">
+            <li>
+                <button
+                    class="btn"
+                    @click="increment">
+                    CLICK HERE! (add count)
+                </button>
+            </li>
+            <li>
+                <button
+                    class="btn"
+                    @[event]="increment">
+                    CLICK HERE! (add count)
+                </button>
+            </li>
+            <li>
+                {{ count }}
+            </li>
+            <li v-once>
+                {{ count }}
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    // 사용할 데이터 명시
+    data() {
+        return {
+            count: 0,
+            event: "click",
+        }
+    },
+    // 사용할 메서드 명시
+    methods: {
+        increment() {
+            // data에 명시한 데이터 지정
+            this.count++
+        },
+    },
+}
+</script>
+
+<style scoped lang="scss">
+ul{
+    &.event{
+        color: royalblue;
+    }
+}
+</style>
