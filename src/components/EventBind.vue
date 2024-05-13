61894cff2cc4a4f893dbe39d5f02f20ad64b6b19
nseulgee <nseulgee@gmail.com>
2024-05-13 04:06:49 +0000

    이벤트 객체 전달, 이벤트 수식어 정리 및 이벤트 버블링 작업중

--- src/components/EventBind.vue
+++ src/components/EventBind.vue
@@ -1,11 +1,26 @@
 <template>
-    <!-- NOTE: 템플릿 문법
-        v-on  : method 에 선언된 함수를 이벤트와 바인딩 시켜준다.
-            -★@이벤트="함수명"     : v-on 생략 형태
-            -★@[변수명]="함수명"   : 이벤트 부분 또한 동적으로 데이터를 전달받을 시 사용
+    <!-- NOTE: [참고] https://v3-docs.vuejs-korea.org/guide/essentials/event-handling.html#key-modifiers
+        v-on       : method 에 선언된 함수를 이벤트와 바인딩 시켜준다.
+            -★@이벤트="함수명"       : v-on 생략 형태
+            -★@[변수명]="함수명"     : 이벤트 부분 또한 동적으로 데이터를 전달받을 시 사용
+            - @이벤트="함수명1(), ...": 한 이벤트에 여러 함수를 바인딩 할 수 있다.
+        $event     : Event 객체를 전달한다.
+            - 기본적으로 함수의 첫번째 인자로 Event 객체가 전달된다.
+            - 함수의 인자를 명시 할 경우 Event 객체는 $event로 전달할 수 있다.
+        이벤트 수식어: 메서드 내에서 세부적인 처리 없이 간편하게 사용하는 v-on 속성 접미사
+            -★.stop  : 이벤트 전파가 중지된다.
+            -★.prevent: 기본 이벤트를 막는다.
+            - .self   : event.target이 엘리먼트 자신일 경우에만 이벤트를 발생시킨다.
+            - .capture: 내부 엘리먼트에서 클릭 이벤트 핸들러가 실행되기 전에, 여기에서 먼저 지정한 핸들러를 발생시킨다.
+            - .once   : 이벤트를 한번만 발생시킨다.
+            - .passive: prevent 설정을 무시하고 이벤트를 무조건 발생시킨다.
+            -★수식어는 연결하여 사용 가능하나 순서가 중요하다!
+                - @click.prevent.self -> 앨리먼트 자체와 그 자식에 대한 클릭의 기본 동작을 방지
+                - @click.self.prevent -> 앨리먼트 자체에 대한 클릭의 기본 동작만 방지
     -->
     <section>
         <h1>이벤트 바인딩</h1>
+        <h2>이벤트 바인딩 기초</h2>
         <ul class="event">
             <li>
                 <button
@@ -28,6 +43,73 @@
                 {{ count }}
             </li>
         </ul>
+
+        <h2>Event 객체</h2>
+        <ul>
+            <li>
+                <button
+                    class="btn"
+                    @click="greeting('hello', $event)">
+                    CLICK HERE! (show alert)
+                </button>
+            </li>
+        </ul>
+
+        <h2>이벤트 수식어</h2>
+        <ul>
+            <li>
+                <a
+                    class="btn"
+                    href="https://naver.com"
+                    target="_blank"
+                    @click="greeting('hello', $event)">
+                    CLICK HERE! (go naver & show alert)
+                </a>
+            </li>
+            <li>
+                <a
+                    class="btn"
+                    href="https://naver.com"
+                    target="_blank"
+                    @click.prevent="greeting('hello', $event)">
+                    CLICK HERE! (!go naver & show alert)
+                </a>
+            </li>
+            <li>
+                <a
+                    class="btn"
+                    href="https://naver.com"
+                    target="_blank"
+                    @click.once="greeting('hello', $event)">
+                    CLICK HERE! (go naver & once show alert)
+                </a>
+            </li>
+            <li class="stopEvent">
+                <h3>이벤트 버블링</h3>
+                <ul
+                    class="parent"
+                    @click="parentHandler">
+                    <li
+                        class="child"
+                        @click="childHandler"></li>
+                </ul>
+                <h3>이벤트 버블링 방지</h3>
+                <ul
+                    class="parent"
+                    @click="parentHandler">
+                    <li
+                        class="child"
+                        @click.stop="childHandler"></li>
+                </ul>
+                <ul
+                    class="parent"
+                    @click.self="parentHandler">
+                    <li
+                        class="child"
+                        @click="childHandler"></li>
+                </ul>
+            </li>
+        </ul>
     </section>
 </template>
 
@@ -46,6 +128,16 @@ export default {
             // data에 명시한 데이터 지정
             this.count++
         },
+        greeting(message, event) {
+            console.log(message, event);
+            alert(message)
+        },
+        parentHandler(){
+            console.log('parentHandler');
+        },
+        childHandler(){
+            console.log('childHandler');
+        }
     },
 }
 </script>
@@ -55,5 +147,24 @@ ul{
     &.event{
         color: royalblue;
     }
+    .stopEvent{
+        .parent{
+            display: block;
+            width: 200px;
+            height: 100px;
+            padding: 20px;
+            box-sizing: border-box;
+            background-color: royalblue;
+            .child{
+                display: block;
+                width: 50%;
+                height: 100%;
+                background-color: orange;
+            }
+        }
+    }
+}
+a{
+    text-decoration: none;
 }
 </style>
